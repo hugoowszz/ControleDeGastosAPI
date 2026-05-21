a9c274255abd276357a973aa933e4093e038a380
Victor Hugo <hugoowvicttor2006@gmail.com>
2026-05-20 22:46:52 -0300

    .

---
+++ src/main/java/org/example/controledegastosapi/ControleDeGastosApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.controledegastosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeGastosApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControleDeGastosApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/controledegastosapi/ControleDeGastosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.controledegastosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeGastosApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
