a1508c722607add3b6ada7e57653bab93dc254d9
Victor Hugo <hugoowvicttor2006@gmail.com>
2026-06-02 00:28:34 -0300

    adding findAllBycategory controller

--- src/main/java/org/example/controledegastosapi/controller/MonthlySummaryController.java
+++ src/main/java/org/example/controledegastosapi/controller/MonthlySummaryController.java
@@ -3,10 +3,7 @@ package org.example.controledegastosapi.controller;
 import org.example.controledegastosapi.entity.MonthlySummary;
 import org.example.controledegastosapi.service.MonthlySummaryService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/summary")
@@ -21,5 +18,10 @@ public class MonthlySummaryController {
     @GetMapping()
     public ResponseEntity<MonthlySummary> calcularGastoPorMesEAno(@RequestParam int month, @RequestParam int year){
         return ResponseEntity.ok(summaryService.gastoMensal(month,year));
+    }
+
+    @GetMapping("/{categoria}")
+    public ResponseEntity<MonthlySummary> calcularGastoPorCategoria(@PathVariable int categoria){
+        return ResponseEntity.ok(summaryService.gastoCategoria(categoria));
     }
 }
