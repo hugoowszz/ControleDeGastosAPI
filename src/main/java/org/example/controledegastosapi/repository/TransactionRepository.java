2b725abeeef6abd5ab5ff500329213ec1811b569
Victor Hugo <hugoowvicttor2006@gmail.com>
2026-06-02 00:28:18 -0300

    adding findAllBycategory query

--- src/main/java/org/example/controledegastosapi/repository/TransactionRepository.java
+++ src/main/java/org/example/controledegastosapi/repository/TransactionRepository.java
@@ -1,5 +1,6 @@
 package org.example.controledegastosapi.repository;
 
+import org.example.controledegastosapi.entity.Category;
 import org.example.controledegastosapi.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,4 +14,9 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long> 
     boolean existsByCategoryId(Long id);
 
     @Query("SELECT e FROM Transaction e WHERE MONTH(e.date) = :month AND YEAR(e.date) = :year")
-    List<Transaction> findAllByMonthAndYear(@Param("month") int month, @Param("year") int year);}
+    List<Transaction> findAllByMonthAndYear(@Param("month") int month, @Param("year") int year);
+
+    List<Transaction> findAllByCategoryId(int category_id);
+
+}
+
